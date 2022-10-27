addcc13eacbe82308203747ea4db20f2a9af5c6b
Deepthi <113082094+abbarajudeepthi@users.noreply.github.com>
2022-10-27 09:15:47 +0530

    Create ShapeDemo.java

---
+++ ShapeDemo.java
@@ -0,0 +1,12 @@
+public class ShapeDemo {
+	public static void main(String args[]) {
+		Rectangle r=new Rectangle(2,6);
+		Triangle t=new Triangle(2,6);
+		Circle c=new Circle(6);
+		r.printArea();
+		t.printArea();
+		c.printArea();
+		
+	}
+
+}
